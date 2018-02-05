974ee2e10a3613c1128e5cbb2b1ba688011c9767
Prem Kumar.K <premkumar.k@skava.com>
2018-02-05 10:20:50 +0530

    Name : Prem Kumar Task : Hunter 132 : String to ASCII

---
+++ Hunter132.java
@@ -0,0 +1,16 @@
+package com.skava.java.guvi;
+
+import java.util.Scanner;
+
+public class Hunter132
+{
+
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        String string = new String();
+        string = scan.next();
+        
+        
+    }
+}
